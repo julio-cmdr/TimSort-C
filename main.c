11229fa5b6d2452ee81bddec49327802127d3ba2
Júlio Resende <julio.cmdr@gmail.com>
2021-03-08 14:44:51 -0300

    Tim Sort

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 
 int main(int argc, char **argv){
-	int length = 100;
+	int length = 200;
 	srand(time(NULL));  
 
 	int *vector = (int*)malloc(sizeof(int)*length);
@@ -19,22 +19,9 @@ int main(int argc, char **argv){
 	}
 	printf("\n\n");
 
-	int *vector2 = (int*)malloc(sizeof(int)*length);
-	
-	memcpy(vector2, vector, sizeof(int)*length);
-	binary_inserction(vector2, length);
-	printf("Binary Inserction\n");
-	printVector(vector2, length);
-
-	memcpy(vector2, vector, sizeof(int)*length);
-	mergeSort(vector2, 0, length-1);
-	printf("\nMerge\n");
-	printVector(vector2, length);
-
 	timSort(vector, length);
 	printf("\nTim\n");
 	printVector(vector, length);
 
 	free(vector);
-	free(vector2);
 }
